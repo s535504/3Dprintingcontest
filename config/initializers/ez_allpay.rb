578797c3088246704ecc0b3e00a78f0eaf84a625
Shane <s5355049@gmail.com>
2016-01-07 23:32:08 +0800

    pushtoheroku16

--- config/initializers/ez_allpay.rb
+++ config/initializers/ez_allpay.rb
@@ -1,7 +1,15 @@
 EzAllpay.setup do |allpay|
+  if Rails.env.development?
     allpay.merchant_id = '2000132'
     allpay.hash_key    = '5294y06JbISpM5x9'
     allpay.hash_iv     = 'v77hoKGq4kWxNNIS'
     allpay.choose_payment = 'Credit'
+    allpay.return_url = 'http://localhost:3000/notify'
+  else
+    allpay.merchant_id = ENV['MERCHANT_ID']
+    allpay.hash_key    = 'ejCk326UnaZWKisg'
+    allpay.hash_iv     = 'q9jcZX8Ib9LM8wYk'
+    allpay.choose_payment = 'Credit'
     allpay.return_url = 'http://tdpcontest.herokuapp.com/notify'
+  end
 end
