00256e7e79f92d78fddd8f7d56fcf25be5820637
Shane <s5355049@gmail.com>
2016-01-10 17:24:53 +0800

    pushtoheroku10

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -10,6 +10,34 @@ class StaticPagesController < ApplicationController
 
   def notify
     @register = Register.new(name:"Van", email:"dark@gmail.com")
-    @register.save
+    if mac_value_ok?
+      @register.save
+    end
   end
+
+  private
+
+    def mac_value_ok?
+      params_copy = params.dup
+
+      params_to_go = params_copy.except(:CheckMacValue, :action, :controller)
+
+      raw_data = params_to_go.sort.map do |x,y|
+        "#{x} = #{y}"
+      end.join('&')
+
+      hash_raw_data = "HashKey=#{EzAllpay.hash_key}&#{raw_data}&HashIV=#{EzAllpay.hash_iv}"
+
+      url_encode_data = (CGI::escape(hash_raw_data)).downcase
+
+      my_mac = Digest::MD5.hexdigest(url_encode_data)
+
+      params_no_mac = params[:CheckMacValue]
+
+      if my_mac ==params_no_mac.to_s.downcase
+        return true
+      else
+        return false
+      end
+    end
 end
