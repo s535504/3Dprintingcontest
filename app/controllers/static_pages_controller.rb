7a2175c7dbbd28d7c9596b82643d71cb3d8fe0e8
Shane <s5355049@gmail.com>
2016-02-11 16:25:52 +0800

    addemail1

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,11 +11,11 @@ class StaticPagesController < ApplicationController
   def notify
     if mac_value_ok?
       Transaction.find_by!(trade_number: params[:MerchantTradeNo]).update!(params: request.POST)
-      @MerchantTradeNo=params[:MerchantTradeNo]
-      sendEmail
-      # id=Transaction.find_by(trade_number: params[:MerchantTradeNo]).register_id
-      # email=Register.find_by(id:id).email
-      # Mail.deliver(from:'LINE DDD',to:email,subject:"3D列印競賽繳費成功通知",body:"您已於歐付寶成功支付LINE DDD主辦3D列印競賽報名費用")
+      # @MerchantTradeNo=params[:MerchantTradeNo]
+      # sendEmail
+      id=Transaction.find_by!(trade_number: params[:MerchantTradeNo]).register_id
+      email=Register.find_by(id:id).email
+      Mail.deliver(from:'LINE DDD',to:email,subject:"3D列印競賽繳費成功通知",body:"您已於歐付寶成功支付LINE DDD主辦3D列印競賽報名費用")
     end
   end
 
