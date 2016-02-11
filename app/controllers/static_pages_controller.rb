efe8e954c399218f26c69c69a7b7e004fd5425ee
Shane <s5355049@gmail.com>
2016-02-11 14:19:01 +0800

    addemail9

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -29,8 +29,6 @@ class StaticPagesController < ApplicationController
   private
 
     def sendEmail(MerchantTradeNo)
-      id=Transaction.find_by(trade_number:MerchantTradeNo).register_id
-      email=Register.find_by(email:id).email
       Mail.deliver(from:'LINE DDD',to:email,subject:"3D列印競賽繳費成功通知",body:"您已於歐付寶成功支付LINE DDD主辦3D列印競賽報名費用")
     end
 
