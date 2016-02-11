1c11a8cd12259203742cea5c4617187856eb8590
Shane <s5355049@gmail.com>
2016-02-11 14:33:39 +0800

    addemail2

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -11,7 +11,8 @@ class StaticPagesController < ApplicationController
   def notify
     if mac_value_ok?
       Transaction.find_by!(trade_number: params[:MerchantTradeNo]).update!(params: request.POST)
-      # sendEmail(params[:MerchantTradeNo])
+      @MerchantTradeNo=params[:MerchantTradeNo]
+      # sendEmail
       # id=Transaction.find_by(trade_number:MerchantTradeNo).register_id
       email=Register.find_by(email:"s5355049@gmail.com").email
       Mail.deliver(from:'LINE DDD',to:email,subject:"3D列印競賽繳費成功通知",body:"您已於歐付寶成功支付LINE DDD主辦3D列印競賽報名費用")
@@ -27,6 +28,12 @@ class StaticPagesController < ApplicationController
   end
 
   private
+
+    def sendEmail
+      id=Transaction.find_by(trade_number:@MerchantTradeNo).register_id
+      email=Register.find_by(email:id).email
+      Mail.deliver(from:'LINE DDD',to:email,subject:"3D列印競賽繳費成功通知",body:"您已於歐付寶成功支付LINE DDD主辦3D列印競賽報名費用")
+    end
 
     def mac_value_ok?
       params_copy = params.dup
