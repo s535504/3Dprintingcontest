ee7bc992d4fd17f5daf0363afc4b4ca34b2be55b
Shane <s5355049@gmail.com>
2016-02-11 12:34:39 +0800

    addemail9

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -12,7 +12,8 @@ class StaticPagesController < ApplicationController
     if mac_value_ok?
       Transaction.find_by!(trade_number: params[:MerchantTradeNo]).update!(params: request.POST)
       id=Transaction.find_by(trade_number: params[:MerchantTradeNo]).register_id
-      Mail.deliver(from:'LINE DDD',to:Register.find_by(email: "s5355049@gmail.com").email,subject:"3D列印競賽繳費成功通知",body:"您已於歐付寶成功支付LINE DDD主辦3D列印競賽報名費用")
+      email=Register.find_by(email:"s5355049@gmail.com").email
+      Mail.deliver(from:'LINE DDD',to:email,subject:"3D列印競賽繳費成功通知",body:"您已於歐付寶成功支付LINE DDD主辦3D列印競賽報名費用")
     end
   end
 
