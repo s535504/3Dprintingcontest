2122594456590cba3fa627ef294e16f214a0949d
Shane <s5355049@gmail.com>
2016-02-20 20:33:04 +0800

    sefewfeeses

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,11 +1,9 @@
-if Rails.env.production?
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
-      :provider              => 'AWS',
-      :aws_access_key_id     => 'AKIAI4J5Q2Q2THV65ZPQ',
-      :aws_secret_access_key => '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',
-      :region => 'us-west-2'
-    }
-    config.fog_directory     =  'ronasbucket'
-  end
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider              => 'AWS',
+    :aws_access_key_id     => 'AKIAI4J5Q2Q2THV65ZPQ',
+    :aws_secret_access_key => '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',
+    :region => 'us-west-2'
+  }
+  config.fog_directory     =  'ronasbucket'
 end
