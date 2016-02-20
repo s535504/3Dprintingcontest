e2703334e149ed155c4b8cbc17f91840f18f6b43
Shane <s5355049@gmail.com>
2016-02-20 20:09:53 +0800

    fsf

--- app/uploaders/igs_uploader.rb
+++ app/uploaders/igs_uploader.rb
@@ -8,7 +8,11 @@ class IgsUploader < CarrierWave::Uploader::Base
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
-  storage :fog
+  if Rails.env.production?
+    storage :fog
+  else
+    storage :fog
+  end
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
