0aa2e1d7afc5beac02d8f35a5e8e458258c462c8
Shane <s5355049@gmail.com>
2016-03-11 20:46:06 +0800

    fesf

--- db/migrate/20160305103324_create_works.rb
+++ db/migrate/20160305103324_create_works.rb
@@ -8,6 +8,6 @@ class CreateWorks < ActiveRecord::Migration
 
       t.timestamps null: false
     end
-    add_index :works,[:displayingwork_id]
+    # add_index :works,[:displayingwork_id]
   end
 end
