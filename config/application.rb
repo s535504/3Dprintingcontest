edae1774f65b204219328bb9fe7f877daf5bdff5
Shane <s5355049@gmail.com>
2016-03-24 15:49:40 +0800

    esfs

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,6 @@ module TDPContest
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.paths << Rails.root.join("app","assets","fonts")
-    I18n.config.available_locales = :zh
-    I18n.config.available_locales = :en
+    I18n.config.available_locales = :zh, :en
   end
 end
