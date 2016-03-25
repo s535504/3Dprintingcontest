352f222e7ec6aa2301654442e090c44791ab3633
Shane <s5355049@gmail.com>
2016-03-25 11:00:08 +0800

    fseg

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   include SessionsHelper
-  before_filter :set_locale
+  before_filter :set_locale, except: :notify
 
   def set_locale
     I18n.locale = extract_locale_from_accept_language_header
