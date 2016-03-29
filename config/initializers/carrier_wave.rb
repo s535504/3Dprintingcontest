4df42bfe8af013a25ffa869dca9f6d213ff95eb1
Shane <s5355049@gmail.com>
2016-03-30 01:38:14 +0800

    sefs

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,9 +1,9 @@
 CarrierWave.configure do |config|
   config.fog_credentials = {
     :provider              => 'AWS',
-    :aws_access_key_id     => 'AKIAJ6AKW7GA6F4OIAIA',
-    :aws_secret_access_key => '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',
-    :region => 'us-west-2'
+    :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+    :aws_secret_access_key => ENV['S3_SECRET_KEY'],
+    :region => ENV['S3_BUCKET_REGION']
   }
-  config.fog_directory     =  'ronasbucket'
+  config.fog_directory     =  ENV['S3_BUCKET']
 end
